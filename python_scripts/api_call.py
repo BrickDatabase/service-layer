167d11cf9c5003736457d46c4f962001b990c137
Edward Riley <55459510+emr9018@users.noreply.github.com>
2021-02-18 11:13:19 -0500

    HTTPBasicAuth added for API Requests
    Will need to have an encrypted password for later use. Another topic for another time.
    
    Co-Authored-By: vanguard714 <28148217+vanguard714@users.noreply.github.com>

--- python_scripts/api_call.py
+++ python_scripts/api_call.py
@@ -1,8 +1,37 @@
-import requests
+##############################################
+# Author:                                    #
+# Edward Riley & Joel Parkerimport requests  #
+##############################################
+
 import json
 
-link = "https://www.reddit.com/reddits.json?limit=99999"
-response = requests.get(link, headers = {'User-agent': 'your bot 0.1'})
 
+# note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
+auth = requests.auth.HTTPBasicAuth('5H8SluqMBijdSA', 'wZAjkKUXYRMD02YnA7XTcKiie3-OaQ')
+# here we pass our login method (password), username, and password
+
+data = {'grant_type': 'password',
+        'username': 'EuropaReport3',
+        'password': 'Dare8devil'}
+# setup our header info, which gives reddit a brief description of our app
+headers = {'User-Agent': 'EuropaReport/0.0.1'}
+
+# send our request for an OAuth token
+res = requests.post('https://www.reddit.com/api/v1/access_token',
+                    auth=auth, data=data, headers=headers)
+# convert response to JSON and pull access_token value
+TOKEN = res.json()['access_token']
+
+# personal use script
+# add authorization to our headers dictionary
+headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+'https://www.reddit.com/dev/api/oauth/'
+
+# while the token is valid (~2 hours) we just add headers=headers to our requests
+response = requests.get('https://oauth.reddit.com/subreddits/popular', headers=headers)
+
+# converts to JS
 responseJSON = json.loads(response.content)
-print(responseJSON)
+prettyJSON = json.dumps(responseJSON, indent=2)
+
+print(prettyJSON)
