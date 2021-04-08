a07f62758a002f6d4d2f4e57860ee6ab89595513
Edward Riley <55459510+emr9018@users.noreply.github.com>
2021-04-08 11:50:24 -0400

    Proper naming file for this file action

---
+++ manual_get_call.py
@@ -0,0 +1,72 @@
+##############################################
+# Author:                                    #
+# Edward Riley                               #
+##############################################
+
+try:
+    import baseAPI
+except:
+    print("baseAPI.py file not found. ")
+    exit(1)
+
+try:
+    import baseSQL
+except:
+    print("baseSQL.py file not found.")
+    exit(1)
+
+try:
+    import apscheduler
+except: 
+    print("apscheduler package not found.")
+
+    
+try:
+    import json
+except:
+    print("JSON package not found.")
+    exit(1)
+
+try:
+    from datetime import datetime
+    import time
+except:
+    print("Datetime or Time not found.")
+    exit(1)
+
+def convertTuple(tup):
+    str =  ''.join(tup)
+    return str
+
+subredditArray = baseSQL.returnSelectAllAbbreviation()
+
+# This job is run everyday at 11:59 AM.
+
+subredditID = 1
+
+for subreddit in subredditArray:
+    
+
+    subreddit = convertTuple(subreddit)
+    # API endpoint will crash if you go too fast, after some tests, 1 second is the optimal speed. 
+    time.sleep(1)
+    print(subredditID)
+    
+    url = "/r/" + subreddit + "/about/"
+    
+    result = (baseAPI.getResult(url))
+    submission = baseAPI.getSubmissionResult(subreddit)['metadata']['total_results']
+    comment = baseAPI.getCommentResult(subreddit)['metadata']['total_results']
+
+    #print(subreddit.capitalize() + " Subscribers:\t" + str(result['data']['subscribers']))
+    #print(subreddit.capitalize() + " Active Users Count:\t" + str(result['data']['active_user_count']))
+    #print(subreddit.capitalize() + " Submission:\t" + str(submission))
+    #print(subreddit.capitalize() + " Comment:\t" + str(comment))
+
+    subscribers = result['data']['subscribers']
+    activeSubscribers = result['data']['active_user_count']
+    date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    baseSQL.insertRowInformation(subredditID, date, subscribers, activeSubscribers, submission, comment)
+    subredditID = subredditID + 1
+
+baseSQL.selectAllInformation()
