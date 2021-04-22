b9d34c2b2099debf7314113833863f95260b3ac7
Edward Riley <55459510+emr9018@users.noreply.github.com>
2021-04-22 18:59:55 -0400

    Adds test_get_call.py file for PyTest
    Co-Authored-By: friedwaffle <28148217+friedwaffle@users.noreply.github.com>

---
+++ test_get_call.py
@@ -0,0 +1,67 @@
+import requests
+import pytest
+try:
+    import baseAPI
+except:
+    print("baseAPI.py file not found. ")
+    exit(1)
+try:
+    import apscheduler
+except: 
+    print("apscheduler package not found.")
+    exit(1)
+try:
+    import numpy
+except:
+    print("numpy package not found")
+    exit(1)
+try:
+    import json
+except:
+    print("JSON package not found.")
+    exit(1)
+try:
+    from datetime import datetime
+    import time
+except:
+    print("Datetime or Time not found.")
+    exit(1)
+def scheduled_job():
+    subredditArray = ['rit','minecraft','bitcoin',
+    'wallstreetbets','robinhood','gamestop',
+    'playstation','xbox','nintendo',
+    'gaming','lsrs']
+    subredditID = 1
+    print(subredditArray)
+    for subreddit in subredditArray:
+        # API endpoint will crash if you go too fast, after some tests, 1 second is the optimal speed. 
+        time.sleep(1)
+        print(subredditID)
+        url = "/r/" + subreddit + "/about/"
+        result = (baseAPI.getResult(url))
+        submission = baseAPI.getSubmissionResult(subreddit)['metadata']['total_results']
+        comment = baseAPI.getCommentResult(subreddit)['metadata']['total_results']
+        subscribers = result['data']['subscribers']
+        activeSubscribers = result['data']['active_user_count']
+        date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        res = requests.post('https://brick-subreddit.herokuapp.com/info', 
+        data={'subreddit_id':subredditID,
+        'submission':submission,
+        'comment':comment,
+        'subscribers':subscribers,
+        'active_subscribers':activeSubscribers,
+        'date':date})
+        print(res)
+        if res.status_code == 200:
+            print('Success!')
+        elif res.status_code == 404:
+            print('Not Found.')
+        subredditID = subredditID + 1
+def testing_op():
+    try:
+        scheduled_job()
+    except:
+        print('Dumping data gone wrong.')
+        raise SystemExit(0)
+def test_mytest():
+        testing_op()
