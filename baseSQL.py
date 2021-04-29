c51e83d6865142cf67cf1ed9ab0af5bf572a85ad
Edward Riley <55459510+emr9018@users.noreply.github.com>
2021-04-28 22:22:42 -0400

    Removes Port

--- baseSQL.py
+++ baseSQL.py
@@ -13,12 +13,11 @@ try:
     database = os.getenv('DB')
     user = os.getenv('DB_USER')
     password = os.getenv('DB_PASSWORD')
-    port = os.getenv('DB_PORT')
 
     # print("host='" + host + "' dbname='" + database + "' user='" + user + "' password='" + password + "' port='" + port + "'")
 
     try:
-        databaseVar = psycopg2.connect("host='" + host + "' dbname='" + database + "' user='" + user + "' password='" + password + "' port='" + port + "'")
+        databaseVar = psycopg2.connect("host='" + host + "' dbname='" + database + "' user='" + user + "' password='" + password)
         databaseVar.autocommit = True
         databaseVar.set_session(autocommit=True)
 
